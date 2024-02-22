584d2a829a8697aa84a88a31ec395b04f33275a1
Jonas Santos <jonashenrique965@gmail.com>
2024-02-22 16:05:34 -0300

    commit

--- src/main/java/br/com/upe/ChefIA/configuration/WebConfig.java
+++ src/main/java/br/com/upe/ChefIA/configuration/WebConfig.java
@@ -1,9 +1,34 @@
 package br.com.upe.ChefIA.configuration;
 
+import br.com.upe.ChefIA.servlet.ChefiaServlet;
+import com.amazon.ask.servlet.ServletConstants;
+import jakarta.servlet.ServletRegistration;
+import jakarta.servlet.http.HttpServlet;
+import org.apache.coyote.Constants;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class WebConfiguration {
+public class WebConfig {
 
-    
+    @Bean
+    public ServletRegistrationBean<HttpServlet> alexaServlet() {
+
+//        loadProperties();
+
+        ServletRegistrationBean<HttpServlet> servRegBean = new ServletRegistrationBean<>();
+        servRegBean.setServlet(new ChefiaServlet());
+        servRegBean.addUrlMappings("/alexa/*");
+        servRegBean.setLoadOnStartup(1);
+        return servRegBean;
+    }
+
+//    private void loadProperties() {
+//        System.setProperty(ServletConstants.TIMESTAMP_TOLERANCE_SYSTEM_PROPERTY, PropertiesUtils.getPropertyValue(ServletConstants.TIMESTAMP_TOLERANCE_SYSTEM_PROPERTY));
+//        System.setProperty(ServletConstants.DISABLE_REQUEST_SIGNATURE_CHECK_SYSTEM_PROPERTY, PropertiesUtils.getPropertyValue(ServletConstants.DISABLE_REQUEST_SIGNATURE_CHECK_SYSTEM_PROPERTY));
+////        System.setProperty(Constants.SSL_KEYSTORE_FILE_PATH_KEY, Constants.SSL_KEYSTORE_FILE_PATH_KEY);
+////        System.setProperty(Constants.SSL_KEYSTORE_PASSWORD_KEY, Constants.SSL_KEYSTORE_PASSWORD_KEY);
+//    }
+
 }
--- src/main/java/br/com/upe/ChefIA/servlet/ChefiaServlet.java
+++ src/main/java/br/com/upe/ChefIA/servlet/ChefiaServlet.java
@@ -1,12 +1,15 @@
 package br.com.upe.ChefIA.servlet;
 
-import br.com.upe.ChefIA.handlers.AskForRecipeIntentHandler;
-import br.com.upe.ChefIA.handlers.LaunchRequestHandler;
+import br.com.upe.ChefIA.handlers.*;
 import com.amazon.ask.Skill;
 import com.amazon.ask.Skills;
 import com.amazon.ask.servlet.SkillServlet;
 
 public class ChefiaServlet extends SkillServlet {
+
+    public ChefiaServlet() {
+        super((getSkill()));
+    }
     private static Skill getSkill() {
         return Skills.standard()
                 .addRequestHandlers(
@@ -16,7 +19,5 @@ public class ChefiaServlet extends SkillServlet {
                 .withSkillId("amzn1.ask.skill.70ec1e59-74b6-4509-9424-879c2f976206")
                 .build();
     }
-    public ChefiaServlet() {
-        super((getSkill()));
-    }
+
 }
