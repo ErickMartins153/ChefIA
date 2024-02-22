0fb6b76082843754f21909eaa4fcc5c8f0d378b3
Jonas Santos <jonashenrique965@gmail.com>
2024-02-22 19:12:59 -0300

    recuperando dados do slot

--- src/main/java/br/com/upe/ChefIA/handlers/AskForRecipeIntentHandler.java
+++ src/main/java/br/com/upe/ChefIA/handlers/AskForRecipeIntentHandler.java
@@ -3,10 +3,14 @@ package br.com.upe.ChefIA.handlers;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
 
+import com.amazon.ask.model.IntentRequest;
 import com.amazon.ask.model.RequestEnvelope;
 import com.amazon.ask.model.Response;
+import com.amazon.ask.model.Slot;
+
 import static com.amazon.ask.request.Predicates.intentName;
 
+import java.util.Map;
 import java.util.Optional;
 
 public class AskForRecipeIntentHandler implements RequestHandler{
@@ -17,6 +21,15 @@ public class AskForRecipeIntentHandler implements RequestHandler{
 
     @Override
     public Optional<Response> handle(HandlerInput input) {
+        RequestEnvelope envelope = input.getRequestEnvelope();
+        Map<String, Slot> request = null;
+        if (envelope.getRequest() instanceof IntentRequest intentRequest) {
+            request = intentRequest.getIntent().getSlots();
+        }
+
+        Slot slot = request.get("ingredientes");
+        System.out.println(slot.getValue());
+
         String speechText = "Poderia repetir, por favor, os ingredientes?";
 
         return input.getResponseBuilder()
