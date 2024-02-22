ed55ac9db0810dea187ad6ba3579a17c79255213
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2024-02-22 14:36:29 -0300

    first commit

---
+++ src/main/java/br/com/upe/ChefIA/ChefIaApplication.java
@@ -0,0 +1,13 @@
+package br.com.upe.ChefIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefIaApplication.class, args);
+	}
+
+}
